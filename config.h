f7b23788e63054e85b4158e0646e96d284ee31ac
Antoine Bertin <antoine@bertin.io>
2023-10-18 22:54:04 +0200

    fix: configuration clean up

--- config.h
+++ config.h
@@ -3,17 +3,21 @@
 #define EE_HANDS
 #define ENCODER_RESOLUTION 2
 
-#ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_SLEEP
-  #define RGBLIGHT_LIMIT_VAL 200
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
 
-  // Animation
-  #define RGBLIGHT_EFFECT_BREATHING
-  #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING
+// Caps Word (see https://docs.qmk.fm/#/feature_caps_word)
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+// RGB Lighting (see https://docs.qmk.fm/#/feature_rgblight)
+#ifdef RGBLIGHT_ENABLE
+  // Configuration
+  #define RGBLIGHT_LIMIT_VAL 200
+  #define RGBLIGHT_SLEEP
+  #define RGBLIGHT_SPLIT
   #define RGBLIGHT_DEFAULT_HUE 112
+
+  // Effects
+  #define RGBLIGHT_EFFECT_BREATHING
   #define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85
   #define RGBLIGHT_EFFECT_BREATHE_MAX RGBLIGHT_LIMIT_VAL
 
@@ -21,5 +25,7 @@
   // #define RGBLIGHT_LAYERS
 #endif
 
-#define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_FORCE_HOLD
+// Tap Hold (see https://docs.qmk.fm/#/tap_hold)
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
--- rules.mk
+++ rules.mk
@@ -1,16 +1,17 @@
 # Configuration
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306
-ENCODER_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-SLEEP_LED_ENABLE = yes
+# Caps Word (https://docs.qmk.fm/#/feature_caps_word)
+CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = no
 
-# Firware size optimization
-MAGIC_ENABLE = no
-GRAVE_ESC_ENABLE = no
-MOUSEKEY_ENABLE = no
-SPACE_CADET_ENABLE = no
+# Firware size optimization (not needed at the moment)
+# see https://docs.qmk.fm/#/squeezing_avr?id=squeezing-the-most-out-of-avr
+# CONSOLE_ENABLE = no
+# COMMAND_ENABLE = no
+# MOUSEKEY_ENABLE = no
+# EXTRAKEY_ENABLE = no
+# SPACE_CADET_ENABLE = no
+# GRAVE_ESC_ENABLE = no
+# MAGIC_ENABLE = no
 
 # Load configuration-specific code
 ifeq ($(strip $(COMBO_ENABLE)), yes)
